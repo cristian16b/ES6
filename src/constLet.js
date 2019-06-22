173b70cbf961a131ccef003c7d27d385cfa1b33a
cristian16b <cristian.budzicz@gmail.com>
2019-06-22 17:48:23 -0300

    Modificado constLet

--- src/constLet.js
+++ src/constLet.js
@@ -1,4 +1,39 @@
-let nombre = 'cristiann';
-let apellido = 'budziczz';
-console.log('nombre = ' + nombre);
-console.log('apellio es ' + apellido);
+// let nombre = 'cristiann';
+// let apellido = 'budziczz';
+// console.log('nombre = ' + nombre);
+// console.log('apellio es ' + apellido);
+
+// const pais = 'arg';
+// const pais = 'argg';
+
+// console.info('pais ' + pais);
+
+// function saludo(){
+//     let nombre = 'dardo';
+//     return 'hola' + nombre;
+// }
+
+// console.log(saludo());
+
+// ambito de variable de tipo bloque con let 
+
+// let edad = 19;
+
+// if(edad>=18) {
+//     let esmayor = true;
+    
+//     console.log(esmayor);  
+// }
+
+//cuando uso let o const
+// let nombre = 'd';nombre='a';
+// console.log('nombre = ' + nombre);
+
+// const nombre = 'd';
+// // nombre='a';
+// console.log('nombre = ' + nombre);
+
+// const con arrays
+const colores = ['rojo','verde'];
+colores.push('azul');
+console.log(colores);
