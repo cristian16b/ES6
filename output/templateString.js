79ada6db05597140c6c3fb2a8637226714765ae3
cristian16b <cristian.budzicz@gmail.com>
2019-06-22 17:52:52 -0300

    Creado stringTemplate

---
+++ output/templateString.js
@@ -0,0 +1,6 @@
+"use strict";
+
+var prueba;
+var nombre = 'cristian g';
+var edad = 29;
+var pais = 'Polonia'; // const salida =
---
+++ src/templateString.js
@@ -0,0 +1,6 @@
+let prueba;    
+const nombre = 'cristian g';
+const edad = 29;
+const pais = 'Polonia';
+
+// const salida = 
