2fc97eb83bd76e8170e3f8f05d56ae5cf25a4e0a
cristian16b <cristian.budzicz@gmail.com>
2019-06-27 20:36:27 -0300

    Mejorado metodo obtenerSuma

--- src/modulos/calculosMatematicos.js
+++ src/modulos/calculosMatematicos.js
@@ -1,9 +1,18 @@
 const ObtenerSuma = () => {
-    let resultado = Number.parseFloat(document.getElementById('numero1').value) 
-                    + 
-                    Number.parseFloat(document.getElementById('numero2').value);
+    // console.log(document.getElementById('formulario').numero1.value)
+    let formulario = document.getElementById('formulario');
+    let a = formulario.numero1.value;
+    let b = formulario.numero2.value;
+    // let resultado = Number.parseFloat(document.getElementById('numero1').value) 
+    //                 + 
+    //                 Number.parseFloat(document.getElementById('numero2').value);
+
+    let resultado = Number.parseFloat(a) + Number.parseFloat(b);
+
     // console.log(resultado);
-    document.getElementById('resultado').value = resultado;
+    // document.getElementById('resultado').value = resultado;
+
+    formulario.resultado.value = resultado;
 }
 
 export {ObtenerSuma};
