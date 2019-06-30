94971d92045e9a237f57f080e7231a58a76a8e56
cristian16b <cristian.budzicz@gmail.com>
2019-06-30 12:36:11 -0300

    Modificado index y obtenerdatos en archivo fetch

--- src/modulos/fetch.js
+++ src/modulos/fetch.js
@@ -8,12 +8,21 @@ let objeto = {
 }
 
 let arrayobjetos = [objeto,objeto,objeto];
-
+let contenido = document.querySelector("#contenido");
 const obtenerDatos = () => {
     fetch('https://randomuser.me/api/')
-        .then(respuesta => {
-            console.log(respuesta);
+        .then(respuesta => respuesta.json())
+        .then(data => {
+            console.log(data.results[0]);
+            contenido.innerHTML =
+            `
+                Nombre ${data.results[0].name.first} - 
+                ${data.results[0].name.last} 
+                - email 
+                ${data.results[0].email}
+            `;
         })
+     //   .catch(console.log('error'))
 }
 
 
