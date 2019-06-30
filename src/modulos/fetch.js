16e4a6d0bae272831541fe64dad2a7bf2dc3891d
cristian16b <cristian.budzicz@gmail.com>
2019-06-30 16:45:36 -0300

    Agregadas funcionalidades en fetch, modificado index

--- src/modulos/fetch.js
+++ src/modulos/fetch.js
@@ -13,7 +13,7 @@ const obtenerDatos = () => {
     fetch('https://randomuser.me/api/')
         .then(respuesta => respuesta.json())
         .then(data => {
-            console.log(data.results[0]);
+            console.log(data.results);
             contenido.innerHTML =
             `
                 Nombre ${data.results[0].name.first} - 
@@ -25,11 +25,38 @@ const obtenerDatos = () => {
      //   .catch(console.log('error'))
 }
 
+let tabla = document.getElementById("tabla");
+
+const actualizarTabla = () => {
+    // subir tabla a un fakejsonserver porque desde local lo rebota por no ser http
+    // fetch('tabla.json')
+    // cambiado a una api de la siguiente web https://jsonplaceholder.typicode.com/
+    fetch('https://jsonplaceholder.typicode.com/todos')
+    .then(respuesta => respuesta.json())
+    .then(data => {
+        cargarFilas(data);
+    })
+ //   .catch(console.log('error'))
+}
+
+const cargarFilas = (datos) => {
+    for(let elemento in datos) {
+        console.log(elemento);
+        tabla.innerHTML += `
+            <tr>
+                <td>${datos[elemento].userId}</td>
+                <td>${datos[elemento].id}</td>
+                <td>${datos[elemento].title}</td>
+                <td>${datos[elemento].completed}</td>
+            </tr>
+        `;
+    }
+}
 
 
 
 
 
 
-export {obtenerDatos};
+export {obtenerDatos,actualizarTabla};
 
