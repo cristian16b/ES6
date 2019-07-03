be7632a123e2dda606c8d5310492ae4f613c3f7e
cristian16b <cristian.budzicz@gmail.com>
2019-07-03 16:50:08 -0300

    Agregado prueba de filter, filter y map combinados

--- src/modulos/mapFiltersReduce.js
+++ src/modulos/mapFiltersReduce.js
@@ -1,14 +1,21 @@
 let almuerzos = [
     {principal: 'arepa', postre: 'helado'},
     {principal: 'tacos', postre: 'chesecake'},
-    {principal: 'pizza', postre: 'galletas'},
+    {principal: 'arepa', postre: 'galletas'},
     {principal: 'sushi', postre: 'quesillo'}
 ];
 
 // uso map
 let platoPrincipal = almuerzos.map((almuerzo) => {
     return almuerzo.principal;
+});
+
+platoPrincipal = almuerzos.filter((almuerzo)=>{
+    return almuerzo.principal === 'arepa';
 })
+.map((almuerzo)=>{
+    return almuerzo.postre;
+});
 
 
 
